21ea54a1d00c21fa563b509ef306929f756c5936
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-17 22:08:53 +0530

    length, arrays

---
+++ array_emp.js
@@ -0,0 +1,9 @@
+function nameLength(names){
+    for(i=0;i<names.length;i++){ //length
+        if(names[i].length>4){
+            console.log(names[i])
+        }
+    }
+}
+let a= ["Joy","Meena","Anne","Xi","Veena"]
+nameLength(a) //invoking function
